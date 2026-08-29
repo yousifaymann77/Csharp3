3dc5ccada3c5ac7dd861f507bbec8ac931b520a5
yousif <yshehab693@gmail.com>
2026-08-29 15:35:11 +0300

    Question 3

--- Assign3/Program.cs
+++ Assign3/Program.cs
@@ -25,8 +25,14 @@
                     Console.WriteLine("Never heard of it");
                     break;
 
-            } 
+            }
             #endregion
+
+            #region 3. Given int pages = 464;, use the ternary operator to store "Long Book" or "Short Book" into a variable sizeLabel (same rule as question 6: long if pages > 300).
+            string sizeLabel = pages > 300 ? "Long Book" : "Short Book";
+            Console.WriteLine(sizeLabel); 
+            #endregion
+
         }
     }
 }
